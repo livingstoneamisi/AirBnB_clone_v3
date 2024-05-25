321b569c1347d62dad6fd96f2d05d6fd34267c7e
Mordecai-Wambua <mordecaingumbau@gmail.com>
2024-05-25 13:45:57 +0300

    api: task 05

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Flask inatance implementation."""
+"""Flask instance implementation."""
 from flask import Flask
 from models import storage
 from api.v1.views import app_views
