5dd6ccc89f94e3bdbfaf8e15d08c97b0912584a1
livingstoneamisi <livingstoneamisi00@gmail.com>
2024-05-25 20:31:18 +0300

    12. HTTP access control (CORS)

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """Flask instance implementation."""
-from flask import Flask
+from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
 from flask_cors import CORS
@@ -9,7 +9,7 @@ app = Flask(__name__)
 app.register_blueprint(app_views)
 
 # Creating a CORS instance allowing /* for 0.0.0.0
-CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
+cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 @app.teardown_appcontext
 def teardown(exc):
@@ -21,12 +21,8 @@ def error(exc):
 
 if __name__ == "__main__":
     from os import getenv
-    host = getenv('HBNB_API_HOST')
-    port = getenv('HBNB_API_PORT')
-    if not host:
-        host = '0.0.0.0'
-    if not port:
-        port = '5000'
-
+    host = getenv('HBNB_API_HOST', '0.0.0.0')
+    port = int(getenv('HBNB_API_PORT', 5000))
+    
     app.run(host=host, port=port, threaded=True)
 
