4e6ff4a4a1ffe73208b4300feb1e5b2419e50f1e
livingstoneamisi <livingstoneamisi00@gmail.com>
2024-05-25 20:20:16 +0300

    12. HTTP access control (CORS)

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,23 +3,21 @@
 from flask import Flask
 from models import storage
 from api.v1.views import app_views
-
+from flask_cors import CORS
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
+# Creating a CORS instance allowing /* for 0.0.0.0
+CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 @app.teardown_appcontext
 def teardown(exc):
     storage.close()
 
-
 @app.errorhandler(404)
 def error(exc):
-    return({
-            "error": "Not found"
-            }, 404)
-
+    return jsonify({"error": "Not found"}), 404
 
 if __name__ == "__main__":
     from os import getenv
@@ -31,3 +29,4 @@ if __name__ == "__main__":
         port = '5000'
 
     app.run(host=host, port=port, threaded=True)
+
