1f6d83add36c2d412a8d54f656e1f3edae5f1238
livingstoneamisi <livingstoneamisi00@gmail.com>
2024-05-25 21:18:45 +0300

    12. HTTP access control (CORS)

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,28 +1,33 @@
 #!/usr/bin/python3
-"""Flask instance implementation."""
+"""
+The RESTful api starts here. The api aids data access in the app.
+"""
+from os import getenv
+
 from flask import Flask, jsonify
-from models import storage
-from api.v1.views import app_views
 from flask_cors import CORS
 
-app = Flask(__name__)
-app.register_blueprint(app_views)
+from api.v1.views import app_views
+from models import storage
 
-# Creating a CORS instance allowing /* for 0.0.0.0
-cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+app.register_blueprint(app_views)
+CORS(app, origins=["0.0.0.0"])
+host = getenv("HBNB_API_HOST", "0.0.0.0")
+port = getenv("HBNB_API_PORT", "5000")
+
 
 @app.teardown_appcontext
-def teardown(exc):
+def teardown(exception):
+    """Cleanup operations"""
     storage.close()
 
+
 @app.errorhandler(404)
-def error(exc):
+def not_found(error):
     return jsonify({"error": "Not found"}), 404
 
-if __name__ == "__main__":
-    from os import getenv
-    host = getenv('HBNB_API_HOST', '0.0.0.0')
-    port = int(getenv('HBNB_API_PORT', 5000))
-    
-    app.run(host=host, port=port, threaded=True)
 
+if __name__ == "__main__":
+    app.run(host, port, threaded=True, debug=True)
