df0aa1b8dd873f8f124a60101c21830b2c1fdd45
Mordecai-Wambua <mordecaingumbau@gmail.com>
2024-05-26 20:52:57 +0300

    change in encryption

--- models/base_model.py
+++ models/base_model.py
@@ -60,7 +60,7 @@ class BaseModel:
         models.storage.new(self)
         models.storage.save()
 
-    def to_dict(self, save_to_file=False):
+    def to_dict(self, save_to_file=None):
         """returns a dictionary containing all keys/values of the instance"""
         new_dict = self.__dict__.copy()
         if "created_at" in new_dict:
@@ -70,7 +70,7 @@ class BaseModel:
         new_dict["__class__"] = self.__class__.__name__
         if "_sa_instance_state" in new_dict:
             del new_dict["_sa_instance_state"]
-        if save_to_file is False:
+        if save_to_file is None:
             if "password" in new_dict:
                 del new_dict["password"]
         return new_dict
